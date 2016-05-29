9855cd3861b92823c861122f78db0032b0bf3883
Shane Brennan <brennash@Shanes-MacBook-Pro.local>
2016-05-29 21:46:52 +0100

    First commit

---
+++ src/GraphCSV.py
@@ -0,0 +1,10 @@
+#################################################################################
+#										#
+# GraphCSV converts CSV input files into a HTML file containing all CSS and 	#
+# JavaScript inline. 								#
+#										#
+#################################################################################
+
+class GraphCSV:
+
+	def __init__(self, csvFilename):
